499fd6983ba196010b9e01cc80a3227d01f3ce42
NguyenTrongDat1753038 <trongdat11ct@gmail.com>
2021-05-28 22:49:53 +0700

    update bot

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const Discord = require('discord.js');
 const crawl = require('./crawler/crawler');
+const keepAlive = require("./server/server")
 require('dotenv').config();
 
 const channel = "news"
@@ -8,8 +9,9 @@ const client = new Discord.Client();
 client.on('error', console.error);
 
 (async () => {
+  keepAlive()
   await client.login(process.env.TOKEN);
   setInterval(() => {
     crawl(client, channel);
-  }, 1000* 60 * 5 );
+  }, 1000 * 60 * 5);
 })();
